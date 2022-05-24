658477250fe0478365cdf7a891764552160a35ce
Sangram <sangram15.behera@gmail.com>
2022-05-25 03:36:20 +0530

    UseCase-1  Respond Happy or Sad

---
+++ MoodAnalyser/MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        string message;
+        public string AnalyseMood(string message)
+        {
+
+            if (message.ToLower().Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+            return message;
+        }
+
+    }
+}
---
+++ MoodAnalyser/MoodAnalyser/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MoodAnalyzerProblem;
+
+Console.WriteLine("Welcome to Mood Analyzer Problem");
+
+MoodAnalyzer analyzer = new();
+string result = analyzer.AnalyseMood("I am in Happy mood");
+Console.WriteLine(result);
