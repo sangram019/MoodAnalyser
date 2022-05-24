405ce6ff02b6b15226c8ce8022cae0d530b5edcb
Sangram <sangram15.behera@gmail.com>
2022-05-25 03:15:58 +0530

    TC-1.1 return Sad

---
+++ MoodAnalyser/UnitTest1/MoodAnalyser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestProject1
+{
+    internal class MoodAnalyzer
+    {
+        private string v;
+
+        public MoodAnalyzer()
+        {
+        }
+
+        public MoodAnalyzer(string v)
+        {
+            this.v = v;
+        }
+
+        internal string AnalyseMood(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string AnalyseMood()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
